3b14775d32316623dea12107471fa06cc1323a26
Daniel Daras <d.j.daras4@gmail.com>
2023-04-10 09:16:57 -0400

    Coffee shop details made

---
+++ vue/src/components/CoffeeShopDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+      <coffee-shop-list />
+      <img src="Images/Filler.jpg" alt="Coffee Cups">
+      <h2>{{coffeeShop.shopName}}</h2>
+      <p>{{coffeeShop.shopLocation}}</p>
+      <p>{{coffeeShop.hasSpirits === true ? "This shop serves alcohol": "This shop does not serve alcohol"}}</p>
+  </section>
+</template>
+
+<script>
+import CoffeeShopList from '../components/CoffeeShopList.vue';
+
+export default {
+    components: {
+        CoffeeShopList
+    }
+}
+</script>
+
+<style>
+
+</style>
