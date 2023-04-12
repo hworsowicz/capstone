ee2d3e3347b945f0ccff4cb53fe02bedc889e3de
Hannah Worsowicz <hworsowicz@gmail.com>
2023-04-12 13:40:25 -0400

    pushing pic grid

--- vue/src/views/CoffeeShopDetails.vue
+++ vue/src/views/CoffeeShopDetails.vue
@@ -13,6 +13,12 @@
     <br><a href="https://www.google.com/maps/place/Stauf's+Coffee+Roasters/@39.9495856,-82.9978752,17z/data=!4m6!3m5!1s0x88388f4b7be30795:0xb4a07466b704923!8m2!3d39.9495856!4d-82.9956865!16s%2Fg%2F1tghq7lx">{{coffeeShop.shopLocation}}</a> </p>
      <p class="price">{{coffeeShop.priceRange}}</p>
      <img class = "menu" src="../Images/staufs menu.jpg" alt="stauf's menu">
+    <div class="picture-grid">
+      <img src="../Images/staufsgvbuilding.jpg" alt="staufs GV building" class="grid-imgs p1">
+      <img src="../Images/staufsmatcha.jpg" alt= "staufs matcha" class="grid-imgs p4">
+      <img src ="../Images/staufscoffee.jpg" alt= "staufs coffee" class="grid-imgs p3">
+      <img src = "../Images/staufslogo.jpg" alt = "logo" class="grid-imgs p2">
+      </div>
     <coffee-shop-card/>
 
 
@@ -49,9 +55,9 @@ div .body{
   font-family: 'Quicksand', sans-serif;
   display: grid;
   grid-template-areas: 
-  "header header header"
-   "map  map  about"
-   "menu menu  menu" ;
+  "header header"
+   "map about"
+   "menu pics";
    row-gap: 5px;
 }
 .subheader {
@@ -95,6 +101,37 @@ img{
 }
 img.menu {
   grid-area: menu;
+}
+.picture-grid{
+  grid-area: pics;
+  display: grid;
+  grid-template-areas: "pic1 pic2"
+                      "pic3 pic4";
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  
+}
+.grid-imgs {
+  grid-area: pic;
+  width: 25vw;
+  display: grid;
+  padding-left: 15px;
+}
+
+.p1 {
+  grid-area: pic1;
+}
+
+.p2 {
+  grid-area: pic2;
+}
+
+.p3 {
+  grid-area: pic3;
+}
+
+.p4 {
+  grid-area: pic4;
 }
 
 </style>
