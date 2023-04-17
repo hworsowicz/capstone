9a153191d51397e1cae3da82d76e8664a96f4a10
Hannah Worsowicz <hworsowicz@gmail.com>
2023-04-17 19:46:28 -0400

    markers are back in town

--- vue/src/components/GoogleMapSearch.vue
+++ vue/src/components/GoogleMapSearch.vue
@@ -1,6 +1,6 @@
 <template>
  <GmapMap
-  :center="{lat:10, lng:10}"
+  :center="{lat:39.9612, lng:-82.9988}"
   :zoom="7"
   map-type-id="terrain"
   style="width: 500px; height: 300px"
@@ -17,8 +17,41 @@
 
 </template>
 <script>
+import CoffeeShopServices from "../services/CoffeeShopServices";
+import {gmapApi} from 'vue2-google-maps'
 
 export default {
+  methods: {
+    
+    getCoffeeShops() {
+      CoffeeShopServices.getAllCoffeeShops()
+        .then((response) => {
+          console.log(response.data);
+          this.$store.commit("SET_COFFEE_SHOPS", response.data);
+        })
+        .catch((err) => console.error("Sorry could not load shops", err));
+    },
+  },
+     created() {
+    this.getCoffeeShops();
+
+  },
+  computed: {
+    google: gmapApi,
+    markers(){
+      if(!this.google)
+      {
+        return []
+      }
+      console.log(this.google)
+     return this.$store.state.coffeeShops.map((c)=>{
+       const obj = {position: new this.google.maps.LatLng(c.latitude, c.longitude)}
+       // console.log(obj)
+       // console.log(obj.position)
+      return obj
+     })
+    }
+  }
 }
 </script>
 <style>
