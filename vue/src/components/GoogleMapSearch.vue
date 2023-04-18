6ab889dc6e37821196a424694aff272e9d9612e3
Hannah Worsowicz <hworsowicz@gmail.com>
2023-04-17 20:41:14 -0400

    user input

--- vue/src/components/GoogleMapSearch.vue
+++ vue/src/components/GoogleMapSearch.vue
@@ -1,28 +1,39 @@
 <template>
- <GmapMap
-  :center="{lat:39.9612, lng:-82.9988}"
-  :zoom="7"
-  map-type-id="terrain"
-  style="width: 500px; height: 300px"
->
-  <GmapMarker
-    :key="index"
-    v-for="(m, index) in markers"
-    :position="m.position"
-    :clickable="true"
-    :draggable="true"
-    @click="center=m.position"
-  />
-</GmapMap>
-
+  <div>
+    <gmap-place-input @place_changed="setPlace" />
+    <GmapMap
+      :center="{ lat: 39.97745, lng: -83.038221 }"
+      :zoom="12"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker :position="userPosition"> </GmapMarker>
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GmapMap>
+  </div>
 </template>
 <script>
 import CoffeeShopServices from "../services/CoffeeShopServices";
-import {gmapApi} from 'vue2-google-maps'
+import { gmapApi } from "vue2-google-maps";
 
 export default {
+  data() {
+    return {
+      place: null,
+    };
+  },
   methods: {
-    
+    setPlace(place) {
+      this.place = place;
+    },
+
     getCoffeeShops() {
       CoffeeShopServices.getAllCoffeeShops()
         .then((response) => {
@@ -32,28 +43,38 @@ export default {
         .catch((err) => console.error("Sorry could not load shops", err));
     },
   },
-     created() {
-    this.getCoffeeShops();
 
+  created() {
+    this.getCoffeeShops();
   },
   computed: {
     google: gmapApi,
-    markers(){
-      if(!this.google)
-      {
-        return []
+    userPosition() {
+      return (
+        this.google &&
+        this.place &&
+        new this.google.maps.LatLng(
+          this.place.geometry.location.lat(),
+          this.place.geometry.location.lng()
+        )
+      );
+    },
+    markers() {
+      if (!this.google) {
+        return [];
       }
-      console.log(this.google)
-     return this.$store.state.coffeeShops.map((c)=>{
-       const obj = {position: new this.google.maps.LatLng(c.latitude, c.longitude)}
-       // console.log(obj)
-       // console.log(obj.position)
-      return obj
-     })
-    }
-  }
-}
+      console.log(this.google);
+      return this.$store.state.coffeeShops.map((c) => {
+        const obj = {
+          position: new this.google.maps.LatLng(c.latitude, c.longitude),
+        };
+        // console.log(obj)
+        // console.log(obj.position)
+        return obj;
+      });
+    },
+  },
+};
 </script>
 <style>
-
 </style>
